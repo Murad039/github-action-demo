831e417cd7cefd0ed97c924d4b452cf4fa2babf6
Murad <83445039+Murad039@users.noreply.github.com>
2025-10-18 17:36:23 +0600

    Fix typo in browser launch method

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 
 def test_example():
   with sync_playwright() as p:
-     browser = p.chromium.lanuch(headless= True)
+     browser = p.chromium.launch(headless= True)
      page = browser.new_page()
      page.goto("https://example.com")
      assert "Example" in page.title()
