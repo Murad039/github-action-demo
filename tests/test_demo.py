007447da3758f7bc88d9a7abbbdc53ee8d366bfb
Murad <83445039+Murad039@users.noreply.github.com>
2025-10-18 17:34:51 +0600

    Update test_demo.py

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -4,6 +4,6 @@ def test_example():
   with sync_playwright() as p:
      browser = p.chromium.lanuch(headless= True)
      page = browser.new_page()
-     page.goto(https://example.com")
+     page.goto("https://example.com")
      assert "Example" in page.title()
      browser.close()
